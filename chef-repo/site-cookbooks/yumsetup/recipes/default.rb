313a84bb64b3cfbe72ccacbcea6ea3278ef1039f
Ajico KIRIHARA <orangepillow@gmail.com>
2013-11-30 18:47:41 +0900

    Add nginx cookbook

--- chef-repo/site-cookbooks/yumsetup/recipes/default.rb
+++ chef-repo/site-cookbooks/yumsetup/recipes/default.rb
@@ -2,6 +2,12 @@ yum_package "yum-fastestmirror" do
   action :install
 end
 
+template "/etc/yum.repos.d/nginx.repo" do
+  owner "root"
+  owner "root"
+  mode "0644"
+end
+
 execute "yum-update" do
   user "root"
   command "yum -y update"
